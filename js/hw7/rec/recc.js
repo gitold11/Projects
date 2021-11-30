8220b1156cd22678994cc808b77b5592a7ef6e36
gitold11 <mykhaylomaksymyshyn@gmail.com>
2021-11-30 20:54:34 +0200

    cw8

--- js/hw7/rec/recc.js
+++ js/hw7/rec/recc.js
@@ -1,9 +1,7 @@
 
 function rec(arr) {
 arr = arr.toString();
-return arr.length === 0
-    ? 0
-    : +arr[0] + rec(arr.slice(1));
+return arr.length === 0 ? 0 : +arr[0] + rec(arr.slice(1));
 };
 let arr = [12345];
 console.log(rec(arr));
